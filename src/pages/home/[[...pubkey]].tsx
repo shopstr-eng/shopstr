d4447774d8406be14406b643eeb5292d9417f7b7
Thomas Yeung <thomas.yeung1003@gmail.com>
2023-07-30 13:03:23 +0800

    style updates to display products in a responsive grid, a cleaner scroll bar and home page view

--- src/pages/home/[[...pubkey]].tsx
+++ src/pages/home/[[...pubkey]].tsx
@@ -3,13 +3,13 @@ import axios from "axios";
 import DisplayEvents from "../components/display-events";
 import { ProductFormValues } from "../components/product-form";
 import { useRouter } from "next/router";
-import { 
-  EnvelopeIcon, 
-  HomeIcon, 
+import {
+  EnvelopeIcon,
+  HomeIcon,
   WalletIcon,
   XCircleIcon,
-  ArrowLeftIcon
-} from '@heroicons/react/24/outline';
+  ArrowLeftIcon,
+} from "@heroicons/react/24/outline";
 import DirectMessages from "../components/direct-messages";
 
 // const Tooltip = ({ content, children }) => {
@@ -68,56 +68,65 @@ const SellerView = () => {
   };
 
   return (
-    <div className="flex flex-col h-screen justify-center items-center bg-yellow-100">
-      <div className="w-10/12 lg:w-2/3 xl:w-1/2 bg-purple-500 rounded-md py-8 px-16">
-        <div className="flex justify-between items-center mb-8">
-          <h1 className="text-3xl font-bold text-yellow-100">
-            {pubkey
-              ? pubkey === localStorage.getItem("publicKey")
-                ? "Your Shopstr Shop"
-                : "Shopstr Seller View"
-              : "Shopstr Marketplace"}
-          </h1>
-          <div className="flex space-x-2">
-            <HomeIcon
-              className={`w-6 h-6 hover:text-purple-700 ${displayComponent === 'home' ? 'text-yellow-100' : ''}`}
-              onClick={() => setDisplayComponent('home')}
-            />
-            <EnvelopeIcon
-              className={`w-6 h-6 hover:text-purple-700 ${displayComponent === 'messages' ? 'text-yellow-100' : ''}`}
-              onClick={() => setDisplayComponent('messages')}
-            />
-            <WalletIcon
-              className={`w-6 h-6 hover:text-purple-700 ${displayComponent === 'wallet' ? 'text-yellow-100' : ''}`}
-              onClick={() => setDisplayComponent('wallet')}
-            />
-          </div>
-        </div>
-        {pubkey ? (
-          <h2 className="max-w-xsm truncate text-yellow-500">
-            <ArrowLeftIcon 
-              className="w-5 h-5 text-yellow-100 hover:text-purple-700" 
-              onClick={() => {
-                routeToShop("");
-              }}
-            >
-              Go Back
-            </ArrowLeftIcon>
-            {pubkey}
-          </h2>
-        ) : undefined}
-        {displayComponent === "home" && (
-          <DisplayEvents
-            router={router}
-            pubkey={pubkey}
-            clickPubkey={(pubkey) => {
-              routeToShop(pubkey);
-            }}
-            handlePostListing={handlePostListing}
+    <div className="xl:w-full h-full bg-purple-500 py-1 px-2 md:py-8 md:px-16">
+      <div className="flex flex-row justify-between">
+        <h1 className="text-3xl font-bold text-yellow-100">
+          {pubkey
+            ? pubkey === localStorage.getItem("publicKey")
+              ? "Your Shopstr Shop"
+              : "Shopstr Seller View"
+            : "Shopstr Marketplace"}
+        </h1>
+        <div className="flex space-x-2">
+          <HomeIcon
+            className={`w-6 h-6 hover:text-purple-700 ${
+              displayComponent === "home" ? "text-yellow-100" : ""
+            }`}
+            onClick={() => setDisplayComponent("home")}
           />
-        )}
-        {displayComponent === "messages" && <DirectMessages />}
+          <EnvelopeIcon
+            className={`w-6 h-6 hover:text-purple-700 ${
+              displayComponent === "messages" ? "text-yellow-100" : ""
+            }`}
+            onClick={() => setDisplayComponent("messages")}
+          />
+          <WalletIcon
+            className={`w-6 h-6 hover:text-purple-700 ${
+              displayComponent === "wallet" ? "text-yellow-100" : ""
+            }`}
+            onClick={() => setDisplayComponent("wallet")}
+          />
+        </div>
       </div>
+      {pubkey ? (
+        <div
+          className="flex flex-row items-center w-fit pr-2 align-middle hover:bg-purple-600 hover:text-black rounded-md cursor-pointer"
+          onClick={() => {
+            routeToShop("");
+          }}
+        >
+          <ArrowLeftIcon
+            className="w-5 h-5 text-purple-700"
+            onClick={() => {
+              routeToShop("");
+            }}
+          >
+            Go Back
+          </ArrowLeftIcon>
+          {pubkey}
+        </div>
+      ) : undefined}
+      {displayComponent === "home" && (
+        <DisplayEvents
+          router={router}
+          pubkey={pubkey}
+          clickPubkey={(pubkey) => {
+            routeToShop(pubkey);
+          }}
+          handlePostListing={handlePostListing}
+        />
+      )}
+      {displayComponent === "messages" && <DirectMessages />}
     </div>
   );
 };
