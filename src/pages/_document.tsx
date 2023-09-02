b9b8a4645c651f920a062c7cbc30b170d096397b
calvadev <32919103+calvadev@users.noreply.github.com>
2023-09-02 22:33:06 +0000

    Added initial Shopstr logo to header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/shopstr.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
