74176109dd385ed987d0045d04075ce25b1e562a
calvadev <32919103+calvadev@users.noreply.github.com>
2023-09-26 03:36:55 +0000

    Added temporary wallet text

--- src/pages/wallet/index.tsx
+++ src/pages/wallet/index.tsx
@@ -1,11 +1,25 @@
+import Link from 'next/link';
+
 const Wallet = () => {
+  const mintUrl = 'https://legend.lnbits.com/cashu/api/v1/AptDNABNBXv8gpuywhx6N';
+  
+  const handleCopyInvoice = () => {
+    navigator.clipboard.writeText(mintUrl);
+    alert('Mint URL copied to clipboard!');
+  };
+  
   return (
-      <div className="flex items-center justify-center h-max mt-[40vh]">
-        <p className="text-4xl text-yellow-100 ">
-          Coming Soon!
-        </p>
-      </div>
+    <div className="flex items-center justify-center h-max mt-[40vh]">
+      <p className="text-3xl text-yellow-100">
+        A native wallet is coming soon! For now, you can claim your tokens for Bitcoin on 
+        <Link href="https://wallet.nutstash.app/" className="hover:text-purple-700">
+          {' '}Nutstash{' '}
+        </Link>
+        using
+        <span className="hover:text-purple-700" onClick={handleCopyInvoice}>{' '}{mintUrl}{' '}</span>
+        as the mint URL.
+      </p>
+    </div>
   );
 };
-
 export default Wallet;
