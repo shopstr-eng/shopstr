e48ad291a6656e5f40c9afa10608a20deaf742a6
calvadev <32919103+calvadev@users.noreply.github.com>
2023-10-16 07:12:08 +0000

    Navbar change

--- pages/components/navbar.tsx
+++ pages/components/navbar.tsx
@@ -12,34 +12,33 @@ const Navbar = () => {
   const router = useRouter();
   return (
     <div className="flex flex-row justify-between">
-      <h1 className="text-3xl font-bold text-yellow-100">Shopstr</h1>
+      <h1
+        className="text-3xl font-bold text-yellow-100 hover:text-purple-700 cursor-pointer"
+        onClick={() => router.push("/marketplace")}
+      >
+        Shopstr
+      </h1>
       <div className="flex space-x-2 mt-2">
-        <HomeIcon
-          className={`w-6 h-6 hover:text-purple-700 ${
-            router.pathname === "/marketplace" ? "text-yellow-100" : ""
-          }`}
-          onClick={() => router.push("/marketplace")}
-        />
         <EnvelopeOpenIcon
-          className={`w-6 h-6 hover:text-purple-700 ${
+          className={`w-6 h-6 hover:text-purple-700 cursor-pointer ${
             router.pathname === "/direct-messages" ? "text-yellow-100" : ""
           }`}
           onClick={() => router.push("/direct-messages")}
         />
         <WalletIcon
-          className={`w-6 h-6 hover:text-purple-700 ${
+          className={`w-6 h-6 hover:text-purple-700 cursor-pointer ${
             router.pathname === "/wallet" ? "text-yellow-100" : ""
           }`}
           onClick={() => router.push("/wallet")}
         />
         <GlobeAltIcon
-          className={`w-6 h-6 hover:text-purple-700 ${
+          className={`w-6 h-6 hover:text-purple-700 cursor-pointer ${
             router.pathname === "/relays" ? "text-yellow-100" : ""
           }`}
           onClick={() => router.push("/relays")}
         />
         <ArrowLeftOnRectangleIcon
-          className="w-6 h-6 hover:text-purple-700"
+          className="w-6 h-6 hover:text-purple-700 cursor-pointer"
           onClick={() => {
             localStorage.removeItem("npub");
             localStorage.removeItem("signIn");
