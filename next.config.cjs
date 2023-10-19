a587f30d24b81206bb2e7db9015b9f317f07fff3
calvadev <32919103+calvadev@users.noreply.github.com>
2023-10-19 00:05:06 +0000

    Added CSP headers

--- next.config.cjs
+++ next.config.cjs
@@ -5,8 +5,31 @@ const withPWA = require("next-pwa");
 const nextConfig = {
   reactStrictMode: true,
   pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          broadcastUpdate: {
+            channelName: 'update',
+          },
+          // Define your own headers here
+          fetchOptions: {
+            headers: {
+              'Content-Security-Policy': "default-src 'self'"
+            },
+          },
+        },
+      },
+    ],
   },
 };
 
