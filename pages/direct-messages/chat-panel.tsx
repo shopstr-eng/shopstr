2ca608f7f8c2e25f4e7135f81397b2fe21e6add2
Thomas Yeung <thomas.yeung1003@gmail.com>
2024-02-20 18:24:13 -0500

    on-enter submits passphrase modal and sends message, up/down arrow keys to traverse chats, escape to exit chat

--- pages/direct-messages/chat-panel.tsx
+++ pages/direct-messages/chat-panel.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { SHOPSTRBUTTONCLASSNAMES } from "../components/utility/STATIC-VARIABLES";
 import {
   ArrowUturnLeftIcon,
+  ArrowsUpDownIcon,
   ChatBubbleLeftIcon,
 } from "@heroicons/react/24/outline";
 import { getLocalStorageData } from "../components/utility/nostr-helper-functions";
@@ -28,7 +29,7 @@ export const ChatPanel = ({
   const [messageInput, setMessageInput] = useState("");
   const [messages, setMessages] = useState<NostrMessageEvent[]>([]); // [chatPubkey, chat]
 
-  const bottomDivRef = useRef();
+  const bottomDivRef = useRef(null);
 
   useEffect(() => {
     setMessages(chatsMap.get(currentChatPubkey)?.decryptedChat || []);
@@ -46,9 +47,20 @@ export const ChatPanel = ({
           <span className="text-5xl text-light-text dark:text-dark-text">
             No Chat Selected
           </span>
+          <div className="flex items-center justify-center gap-3 pt-5 opacity-10">
+            <span className="text-2xl text-light-text dark:text-dark-text">
+              Use your up and down arrow keys to select chats!
+            </span>
+            <ArrowsUpDownIcon className="h-10 w-10 text-light-text dark:text-dark-text" />
+          </div>
         </div>
       </div>
     );
+
+  const sendMessage = () => {
+    handleSendMessage(messageInput);
+    setMessageInput("");
+  };
 
   return (
     <div className="absolute z-20 flex h-[85vh] w-full flex-col overflow-clip px-2 dark:bg-dark-bg md:relative">
@@ -77,24 +89,28 @@ export const ChatPanel = ({
       </div>
       <div className="space-x flex items-center p-2">
         <Input
-          className="text-light-text dark:text-dark-text"
+          className="pr-3 text-light-text dark:text-dark-text"
           type="text"
           width="100%"
-          size="large"
+          size="md"
           value={messageInput}
           placeholder="Type your message..."
           onChange={(e) => {
             setMessageInput(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (
+              e.key === "Enter" &&
+              !(messageInput === "" || isSendingDMLoading)
+            )
+              sendMessage();
           }}
         />
         <Button
           className={SHOPSTRBUTTONCLASSNAMES}
           isDisabled={messageInput === "" || isSendingDMLoading}
           isLoading={isSendingDMLoading}
-          onClick={() => {
-            handleSendMessage(messageInput);
-            setMessageInput("");
-          }}
+          onClick={sendMessage}
         >
           Send
         </Button>
