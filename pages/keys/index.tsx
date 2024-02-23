9b6a95cdf47f33b0123badeafa02f964f885c29f
calvadev⚡️ <32919103+calvadev@users.noreply.github.com>
2024-02-23 07:33:29 +0000

    CSS fix

--- pages/keys/index.tsx
+++ pages/keys/index.tsx
@@ -73,7 +73,7 @@ const Keys = () => {
   };
 
   return (
-    <div className="flex h-full flex-col bg-light-bg pb-20 pt-4 dark:bg-dark-bg sm:ml-[120px] md:ml-[250px]">
+    <div className="flex h-[100vh] flex-col bg-light-bg pb-20 pt-4 dark:bg-dark-bg sm:ml-[120px] md:ml-[250px]">
       <div className="p-4">
         <Card>
           <CardBody>
