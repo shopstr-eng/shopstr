16d4853697bab497dffb11080fb83683b1be63c4
calvadev⚡️ <32919103+calvadev@users.noreply.github.com>
2024-04-16 20:36:11 +0000

    Changed action after mint payment

--- components/wallet/mint-button.tsx
+++ components/wallet/mint-button.tsx
@@ -107,9 +107,8 @@ const MintButton = () => {
           setPaymentConfirmed(true);
           setQrCodeUrl(null);
           setTimeout(() => {
-            router.push("/"); // takes you back to the home page after payment has been confirmed by cashu mint api
+            handleToggleMintModal(); // takes you back to the page after payment has been confirmed by cashu mint api
           }, 1900); // 1.9 seconds is the amount of time for the checkmark animation to play
-          handleToggleMintModal();
           break;
         }
       } catch (error) {
