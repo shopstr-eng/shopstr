2e9ac6badb32df535c1fc787621d9dc6b41a2658
Shopstr-LiquidDev <>
2024-08-06 14:21:04 +0200

    chore: accomodate config for lwk_wasm

---
+++ next.config.js
@@ -0,0 +1,40 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.output.environment = {
+        ...config.output.environment,
+        asyncFunction: true,
+      };
+    }
+
+    config.experiments = {
+      asyncWebAssembly: true,
+      syncWebAssembly: true,
+      layers: true,
+    };
+
+    return config;
+  },
+
+  images: {
+    domains: [
+      "www.google.com",
+      "www.facebook.com",
+      "www.twitter.com",
+      "www.instagram.com",
+      "duckduckgo.com",
+      "www.youtube.com",
+      "www.pinterest.com",
+      "www.linkedin.com",
+      "www.reddit.com",
+      "www.quora.com",
+      "www.wikipedia.org",
+    ],
+  },
+};
+
+export default nextConfig;
