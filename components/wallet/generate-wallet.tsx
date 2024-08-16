7f6100b0068cb4e82458de0708b95e9d65b6d637
Shopstr-LiquidDev <>
2024-08-16 18:23:19 +0200

    feat: new ui for seed showcase

--- components/wallet/generate-wallet.tsx
+++ components/wallet/generate-wallet.tsx
@@ -3,13 +3,18 @@ import { SHOPSTRBUTTONCLASSNAMES } from "../utility/STATIC-VARIABLES"
 import { validPassphrase } from "../utility/nostr-helper-functions";
 import { encryptWalletToLocalStorageWithPassphrase, generateLiquidDescriptor, generateNewMnemonic, generateNewSigner, getNetwork } from "@/pages/wallet/lib";
 import { useWalletContext } from "./wallet-context";
+import { Dialog, DialogContent } from "../utility/shadcn/Dialog";
+import { useState } from "react";
+import { DialogTitle } from "@radix-ui/react-dialog";
 
 interface GenerateWalletProps {
   onGenerateSuccess: (passphrase: string) => void;
 }
 
 export const GenerateWallet = ({ onGenerateSuccess }: GenerateWalletProps ) => {
-  const { changePassphrase } = useWalletContext();
+  const { changePassphrase, passphrase, descriptor, changeDescriptor } = useWalletContext();
+  const [mnemonic, setMnemonic] = useState<string[]>([]);
+  const [showSeedDialog, setShowSeedDialog] = useState(false);
 
   const handleGenerateWallet = () => {
     let isPassphraseValid = false;
@@ -25,15 +30,23 @@ export const GenerateWallet = ({ onGenerateSuccess }: GenerateWalletProps ) => {
         const mnemonic = generateNewMnemonic();
         const signer = generateNewSigner(mnemonic, network);
         const descriptor = generateLiquidDescriptor(signer);
-
-        encryptWalletToLocalStorageWithPassphrase(passphrase, descriptor);
-        alert(`Keep these words safe, for recovery! \n\nRecovery phrase:\n****\n${mnemonic}\n****`);
-        onGenerateSuccess(passphrase);
+        changeDescriptor(descriptor);
+        setMnemonic(mnemonic?.split(" "));
+        setShowSeedDialog(true);
       } else {
         alert("Incorrect passphrase.");
       }
     }
   }
+
+  const handleConfirm = () => {
+    const confirmAgain = window.confirm("Are you really sure?");
+    if (confirmAgain) {
+      encryptWalletToLocalStorageWithPassphrase(passphrase, descriptor);
+      onGenerateSuccess(passphrase);
+    }
+  }
+
   return (
     <div className="w-max mx-auto flex flex-col items-center">
       <h2 className="text-4xl font-bold">You don't have a wallet yet!</h2>
@@ -43,6 +56,43 @@ export const GenerateWallet = ({ onGenerateSuccess }: GenerateWalletProps ) => {
       >
         Generate a New Wallet
       </Button>
+      <Dialog open={showSeedDialog} onOpenChange={(val) => setShowSeedDialog(val)}>
+        <DialogContent>
+          <DialogTitle className="flex flex-col gap-y-3">
+            <p className="text-2xl font-bold text-white">Secure your recovery phrase</p>
+            <p className="text-muted">
+              The combination of words bellow are called your recovery phrase. The recovery phrase allows you to
+              access and restore your wallet. Write them down on a piece of paper in the exact order.
+            </p>
+          </DialogTitle>
+          <main className="flex flex-col justify-center items-center gap-y-4">
+            <section className="grid grid-cols-2 gap-x-4">
+              <div className="flex flex-col gap-y-2">
+                { mnemonic && mnemonic?.slice(0, 6)?.map((word, i) => (
+                    <span className="text-lg font-bold text-white" key={i}>{i + 1}: {word}</span>
+                  ))
+                }
+              </div>
+              <div className="flex flex-col gap-y-2">
+                { mnemonic && mnemonic?.slice(6)?.map((word, i) => (
+                    <span className="text-lg font-bold text-white" key={i}>{i + 6 + 1}: {word}</span>
+                  ))
+                }
+              </div>
+            </section>
+            <section className="flex flex-col gap-y-3">
+              <p className="text-muted">
+                If  you lose it or write it down incorrectly, you will permanently lose access to your funds.
+                Do not photograph the recovery phrase and do not store it digitally.
+              </p>
+              <p className="text-lg font-bold">The recovery phrase IS NOT stored on our servers, and we cannot help you recover it.</p>
+            </section>
+            <footer>
+              <Button className="text-lg h-min px-6 py-2 rounded bg-cyan-500" onClick={() => handleConfirm()}>I have written it down safely!</Button>
+            </footer>
+          </main>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
