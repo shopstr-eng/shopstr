11dbc37e80938465423200fc92faec50f179ab10
calvadev⚡️ <32919103+calvadev@users.noreply.github.com>
2024-09-01 19:53:26 +0000

    Change product summary position in product view

--- components/utility-components/checkout-card.tsx
+++ components/utility-components/checkout-card.tsx
@@ -34,7 +34,6 @@ export default function CheckoutCard({
 
   const [isExpanded, setIsExpanded] = useState(false);
   const [isBeingPaid, setIsBeingPaid] = useState(false);
-  const [expandedImageSrc, setExpandedImageSrc] = useState<string | null>(null);
   const [visibleImages, setVisibleImages] = useState<string[]>([]);
   const [showAllImages, setShowAllImages] = useState(false);
   const [selectedImage, setSelectedImage] = useState(images[0]);
@@ -78,92 +77,98 @@ export default function CheckoutCard({
     setIsBeingPaid(!isBeingPaid);
   };
 
-  const handleImageClick = (src: string) => {
-    setExpandedImageSrc(src);
-  };
-
-  const closeModal = () => {
-    setExpandedImageSrc(null);
-  };
-
   return (
     <>
-      {/* {expandedImageSrc && (
-        <div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
-          onClick={closeModal}
-        >
-          <img
-            src={expandedImageSrc}
-            alt="Expanded product"
-            className="max-h-full max-w-full"
-          />
-        </div>
-      )} */}
       {!isBeingPaid ? (
-        <div
-          className="mx-[2.5px] my-3 flex h-[99%] w-[99%] w-full flex-row"
-          key={uniqueKey}
-        >
-          <div className="w-2/3 pr-4">
-            <div className="flex w-full flex-row">
-              <div className="flex w-1/4 flex-col pr-4">
-                <div ref={containerRef} className="flex-1 overflow-hidden">
-                  <div
-                    className={`flex flex-col space-y-2 ${
-                      showAllImages ? "overflow-y-auto" : ""
-                    }`}
-                  >
-                    {(showAllImages ? images : visibleImages).map(
-                      (image, index) => (
-                        <img
-                          key={index}
-                          src={image}
-                          alt={`Product image ${index + 1}`}
-                          className={`w-full cursor-pointer object-cover ${
-                            image === selectedImage
-                              ? "border-2 border-shopstr-purple dark:border-shopstr-yellow"
-                              : ""
-                          }`}
-                          style={{ aspectRatio: "1 / 1" }}
-                          onClick={() => setSelectedImage(image)}
-                        />
-                      ),
-                    )}
+        <div className="h-[100%] w-[100%]">
+          <div
+            className="max-w-screen mx-3 my-3 flex flex-row whitespace-normal break-words"
+            key={uniqueKey}
+          >
+            <div className="w-2/3 pr-4">
+              <div className="flex w-full flex-row">
+                <div className="flex w-1/4 flex-col pr-4">
+                  <div ref={containerRef} className="flex-1 overflow-hidden">
+                    <div
+                      className={`flex flex-col space-y-2 ${
+                        showAllImages ? "overflow-y-auto" : ""
+                      }`}
+                    >
+                      {(showAllImages ? images : visibleImages).map(
+                        (image, index) => (
+                          <img
+                            key={index}
+                            src={image}
+                            alt={`Product image ${index + 1}`}
+                            className={`w-full cursor-pointer object-cover ${
+                              image === selectedImage
+                                ? "border-2 border-shopstr-purple dark:border-shopstr-yellow"
+                                : ""
+                            }`}
+                            style={{ aspectRatio: "1 / 1" }}
+                            onClick={() => setSelectedImage(image)}
+                          />
+                        ),
+                      )}
+                    </div>
                   </div>
+                  {images.length > visibleImages.length && (
+                    <button
+                      onClick={() => setShowAllImages(!showAllImages)}
+                      className="mt-2 text-sm text-purple-500 hover:text-purple-700 dark:text-yellow-500 dark:hover:text-yellow-700"
+                    >
+                      {showAllImages ? "∧" : "∨"}
+                    </button>
+                  )}
                 </div>
-                {images.length > visibleImages.length && (
+                <div className="w-3/4">
+                  <img
+                    src={selectedImage}
+                    alt="Selected product image"
+                    className="w-full object-cover"
+                    style={{ aspectRatio: "1 / 1" }}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="w-1/3 p-3">
+              <ProfileWithDropdown
+                pubkey={pubkey}
+                dropDownKeys={
+                  pubkey === getLocalStorageData().userPubkey
+                    ? ["shop_settings"]
+                    : ["shop", "message"]
+                }
+              />
+              <h2 className="mt-4 w-full text-left text-2xl font-bold text-light-text dark:text-dark-text">
+                {title}
+              </h2>
+              <div className="hidden sm:block">
+                <p className="mt-4 w-full text-left text-lg text-light-text dark:text-dark-text">
+                  {renderSummary()}
+                </p>
+                {summary.length > SUMMARY_CHARACTER_LIMIT && (
                   <button
-                    onClick={() => setShowAllImages(!showAllImages)}
-                    className="mt-2 text-sm text-purple-500 hover:text-purple-700 dark:text-yellow-500 dark:hover:text-yellow-700"
+                    onClick={toggleExpand}
+                    className="mt-2 text-purple-500 hover:text-purple-700 dark:text-yellow-500 dark:hover:text-yellow-700"
                   >
-                    {showAllImages ? "∧" : "∨"}
+                    {isExpanded ? "Show less" : "Show more"}
                   </button>
                 )}
               </div>
-              <div className="w-3/4">
-                <img
-                  src={selectedImage}
-                  alt="Selected product image"
-                  className="w-full object-cover"
-                  style={{ aspectRatio: "1 / 1" }}
-                />
+              <div className="mt-4">
+                <CompactPriceDisplay monetaryInfo={productData} />
               </div>
+              <Button
+                className={SHOPSTRBUTTONCLASSNAMES}
+                onClick={toggleBuyNow}
+              >
+                Buy Now
+              </Button>
             </div>
           </div>
-          <div className="w-1/3 p-3">
-            <ProfileWithDropdown
-              pubkey={pubkey}
-              dropDownKeys={
-                pubkey === getLocalStorageData().userPubkey
-                  ? ["shop_settings"]
-                  : ["shop", "message"]
-              }
-            />
-            <h2 className="mt-4 w-full text-left text-2xl font-bold text-light-text dark:text-dark-text">
-              {title}
-            </h2>
-            <p className="mt-4 w-full text-left text-lg text-light-text dark:text-dark-text">
+          <div className="max-w-screen mx-3 my-3 max-w-full overflow-hidden whitespace-normal break-words sm:hidden">
+            <p className="break-words-all w-full text-left text-lg text-light-text dark:text-dark-text">
               {renderSummary()}
             </p>
             {summary.length > SUMMARY_CHARACTER_LIMIT && (
@@ -174,12 +179,6 @@ export default function CheckoutCard({
                 {isExpanded ? "Show less" : "Show more"}
               </button>
             )}
-            <div className="mt-4">
-              <CompactPriceDisplay monetaryInfo={productData} />
-            </div>
-            <Button className={SHOPSTRBUTTONCLASSNAMES} onClick={toggleBuyNow}>
-              Buy Now
-            </Button>
           </div>
         </div>
       ) : (
