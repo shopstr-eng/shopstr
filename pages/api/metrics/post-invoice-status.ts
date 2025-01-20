220c35424b5f811fa2567c75a2d08286c712251a
calvadev⚡️ <32919103+calvadev@users.noreply.github.com>
2025-01-20 08:26:59 +0000

    Removed unnecessary mint check when posting order metric

--- pages/api/metrics/post-invoice-status.ts
+++ pages/api/metrics/post-invoice-status.ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { DateTime } from "luxon";
 import { v4 as uuid } from "uuid";
-import { CashuMint, CashuWallet } from "@cashu/cashu-ts";
 import repo from "../../../utils/metrics/repo";
 import {
   locationToSqlGeo,
@@ -21,42 +20,27 @@ const UpdateInvoice = async (req: NextApiRequest, res: NextApiResponse) => {
 
   const event = parseRequestBody(req.body);
 
-  const wallet = new CashuWallet(new CashuMint(event.mint));
-
-  if (!event.id || !event.listing_id) {
-    return res.status(400).json({});
-  }
-
-  try {
-    await wallet.mintProofs(event.total, event.hash);
-  } catch (error: any) {
-    console.error(error);
-    if (error.message.includes("quote already issued")) {
-      await repo()("transactions").insert({
-        id: uuid(),
-        date_time: DateTime.now().toUTC().toSQL(),
-        total: event.total,
-        sub_total: event.sub_total,
-        tip_total: event.tip_total,
-        shipping_total: event.shipping_total,
-        discount_total: event.discount_total,
-        fee_total: event.fee_total,
-        tax_total: event.tax_total,
-        currency: event.currency,
-        merchant_id: event.merchant_id,
-        listing_id: event.listing_id,
-        funding_source: "ln",
-        customer_location: locationToSqlGeo(
-          await getLocationFromReqHeaders(req.headers),
-        ),
-        merchant_location: locationToSqlGeo(
-          await getLocationFromAddress(event.merchant_location),
-        ),
-      });
-      return res.status(200).json({});
-    }
-    throw error;
-  }
+  await repo()("transactions").insert({
+    id: uuid(),
+    date_time: DateTime.now().toUTC().toSQL(),
+    total: event.total,
+    sub_total: event.sub_total,
+    tip_total: event.tip_total,
+    shipping_total: event.shipping_total,
+    discount_total: event.discount_total,
+    fee_total: event.fee_total,
+    tax_total: event.tax_total,
+    currency: event.currency,
+    merchant_id: event.merchant_id,
+    listing_id: event.listing_id,
+    funding_source: "ln",
+    customer_location: locationToSqlGeo(
+      await getLocationFromReqHeaders(req.headers),
+    ),
+    merchant_location: locationToSqlGeo(
+      await getLocationFromAddress(event.merchant_location),
+    ),
+  });
   return res.status(201).json({});
 };
 
