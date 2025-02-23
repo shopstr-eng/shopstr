e6af19cf22c278d3a0abc795a45406e1244d0497
calvadev⚡️ <32919103+calvadev@users.noreply.github.com>
2025-02-23 23:19:55 +0000

    Added window.location.origin handling, fixed dropdown spacing issue, and changed placeholder text

--- components/home/marketplace.tsx
+++ components/home/marketplace.tsx
@@ -347,8 +347,8 @@ export function MarketplacePage({
             </div>
           </div>
         ) : (
-          <>
-            <div className="flex flex-row gap-2 pb-3">
+          <div className="flex flex-col gap-2 pb-3 sm:flex-row">
+            <div className="w-full">
               <Input
                 className="mt-2 text-light-text dark:text-dark-text"
                 isClearable
@@ -360,6 +360,8 @@ export function MarketplacePage({
                   setSelectedSearch(value);
                 }}
               ></Input>
+            </div>
+            <div className="flex w-full flex-row gap-2 pb-3">
               <Select
                 className="mt-2 text-light-text dark:text-dark-text"
                 label="Categories"
@@ -400,7 +402,7 @@ export function MarketplacePage({
                 />
               ) : null}
             </div>
-          </>
+          </div>
         )}
       </div>
       <div className="flex">
