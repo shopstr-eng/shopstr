e2869f7f9c5c5e4c81e9698ec6bcaa72c10057e9
calvadev⚡️ <32919103+calvadev@users.noreply.github.com>
2025-03-26 11:34:48 +0000

    Changed file extensions

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-/** @type {import('next').NextConfig} */
+import { NextConfig } from "next";
 
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
   runtimeCaching: [
     {
       // Cache static assets
@@ -47,7 +46,7 @@ const withPWA = require("next-pwa")({
   ],
 });
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   output: "standalone",
   reactStrictMode: true,
   images: {
