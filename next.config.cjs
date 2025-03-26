00e36b34e87b32222d61988a98f8dc23adc2c335
calvadev⚡️ <32919103+calvadev@users.noreply.github.com>
2025-03-26 14:21:09 +0000

    Next.js config changes

--- next.config.cjs
+++ next.config.cjs
@@ -4,6 +4,7 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
   runtimeCaching: [
     {
       // Cache static assets
