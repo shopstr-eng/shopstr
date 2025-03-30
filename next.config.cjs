ae662e652284cc38f75d9b3eeb4912252866974f
Abhijit-Sahoo <abhijit09040@gmail.com>
2025-03-30 22:58:41 +0530

    Update next.config.cjs

--- next.config.cjs
+++ next.config.cjs
@@ -65,6 +65,33 @@ const nextConfig = {
       "www.wikipedia.org",
     ],
   },
+  webpack: (config, { isServer }) => {
+    // Optimize chunk loading
+    config.optimization = {
+      ...config.optimization,
+      splitChunks: {
+        chunks: 'all',
+        minSize: 20000,
+        maxSize: 244000,
+        minChunks: 1,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            reuseExistingChunk: true,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+        },
+      },
+    };
+    return config;
+  },
 };
 
 module.exports = withPWA(nextConfig);
